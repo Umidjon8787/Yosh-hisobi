9417fef93ec62d0516b6d5d4451e25cd76fb11c3
Umidjon8787 <100425651+Umidjon8787@users.noreply.github.com>
2022-04-18 18:07:49 +0300

    вапвап

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,18 @@
 function ageInDays() {
     let birthYear = prompt("Tug'ilgan yilingizni kiriting");
-    let ageInDays = (2022 - birthYear) * 365;
-    let h1 = document.createElement('h1');
-    let textAnswer = document.createTextNode('Siz jami' + ' ' + ageInDays + ' ' + 'Kun yashadingiz.');
+    if (birthYear > 0 ){
+        let ageInDays = (2022 - birthYear) * 365;
+        let h1 = document.createElement('h1');
+        let textAnswer = document.createTextNode('Siz jami' + ' ' + ageInDays + ' ' + 'Kun yashadingiz.');
 
-    h1.setAttribute('id','ageIndays');
-    h1.appendChild(textAnswer);
+        h1.setAttribute('id','ageIndays');
+        h1.appendChild(textAnswer);
 
-    document.getElementById('result').appendChild(h1)
+        document.getElementById('result').appendChild(h1)
+    } else{
+        alert("Tugilgan sanani kiriting")
+    }
+    
     
 }
 
